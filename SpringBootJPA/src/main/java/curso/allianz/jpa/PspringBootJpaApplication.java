eba4ef7cb8de471262cb788c945a17393dd6440a
Tu Nombre <you@example.com>
2021-05-19 13:20:43 +0200

    carga inicial

---
+++ SpringBootJPA/src/main/java/curso/allianz/jpa/PspringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package curso.allianz.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspringBootJpaApplication.class, args);
+	}
+
+}
---
+++ SpringBootJPA/src/test/java/curso/allianz/jpa/PspringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.allianz.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
